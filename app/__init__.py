e0786726bf2b19e9231a097b78c2a01978e57462
Muthomi Mate <muthomimate@gmail.com>
2017-09-25 21:48:08 +0300

    [Refactor]change put delete and get routes to allow auth

--- app/__init__.py
+++ app/__init__.py
@@ -67,39 +67,64 @@ def create_app(config_name):
                 }
                 return make_response(jsonify(response)), 401
 	@app.route('/shoppinglists/<int:id>', methods=['GET', 'PUT', 'DELETE'])
-	def shoppinglist_manipulation(id, **kwargs):
-		# retrieve a buckelist using it's ID
-		shoppinglist = Shoppinglist.query.filter_by(id=id).first()
-		if not shoppinglist:
-			# Raise an HTTPException with a 404 not found status code
-			abort(404)
-		if request.method == 'DELETE':
-			shoppinglist.delete()
-			return {
-			"message": "shoppinglist {} deleted successfully".format(shoppinglist.id)
-			}, 200
-		elif request.method == 'PUT':
-			name = str(request.data.get('name', ''))
-			shoppinglist.name = name
-			shoppinglist.save()
-			response = jsonify({
-				'id': shoppinglist.id,
-				'name': shoppinglist.name,
-				'date_created': shoppinglist.date_created,
-				'date_modified': shoppinglist.date_modified
-			})
-			response.status_code = 200
-			return response
-		else:
-			 # GET
-			 response = jsonify({
-			 	'id': shoppinglist.id,
-			 	'name': shoppinglist.name,
-			 	'date_created': shoppinglist.date_created,
-			 	'date_modified': shoppinglist.date_modified
-			 })
-			 response.status_code = 200
-			 return response
+    def shoppinglist_manipulation(id, **kwargs):
+        # get the access token from the authorization header
+        auth_header = request.headers.get('Authorization')
+        access_token = auth_header.split(" ")[1]
+
+        if access_token:
+            # Get the user id related to this access token
+            user_id = User.decode_token(access_token)
+
+            if not isinstance(user_id, str):
+                # If the id is not a string(error), we have a user id
+                # Get the shoppinglist with the id specified from the URL (<int:id>)
+                shoppinglist = Shoppinglist.query.filter_by(id=id).first()
+                if not shoppinglist:
+                    # There is no shoppinglist with this ID for this User, so
+                    # Raise an HTTPException with a 404 not found status code
+                    abort(404)
+
+                if request.method == "DELETE":
+                    # delete the shoppinglist using our delete method
+                    shoppinglist.delete()
+                    return {
+                        "message": "shoppinglist {} deleted".format(shoppinglist.id)
+                    }, 200
+
+                elif request.method == 'PUT':
+                    # Obtain the new name of the shoppinglist from the request data
+                    name = str(request.data.get('name', ''))
+
+                    shoppinglist.name = name
+                    shoppinglist.save()
+
+                    response = {
+                        'id': shoppinglist.id,
+                        'name': shoppinglist.name,
+                        'date_created': shoppinglist.date_created,
+                        'date_modified': shoppinglist.date_modified,
+                        'created_by': shoppinglist.created_by
+                    }
+                    return make_response(jsonify(response)), 200
+                else:
+                    # Handle GET request, sending back the shoppinglist to the user
+                    response = {
+                        'id': shoppinglist.id,
+                        'name': shoppinglist.name,
+                        'date_created': shoppinglist.date_created,
+                        'date_modified': shoppinglist.date_modified,
+                        'created_by': shoppinglist.created_by
+                    }
+                    return make_response(jsonify(response)), 200
+            else:
+                # user is not legit, so the payload is an error message
+                message = user_id
+                response = {
+                    'message': message
+                }
+                # return an error response, telling the user he is Unauthorized
+                return make_response(jsonify(response)), 401
 	# import the authentication blueprint and register it on the app
 	from .auth import auth_blueprint
 	app.register_blueprint(auth_blueprint)
