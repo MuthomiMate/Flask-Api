32644ae37486d8a1a1589a01cfe293f05698bc2a
Muthomi Mate <muthomimate@gmail.com>
2017-10-06 17:40:26 +0300

    [refactor]fix test issue

--- app/__init__.py
+++ app/__init__.py
@@ -19,7 +19,7 @@ db = SQLAlchemy()
 def create_app(config_name):
     from app.models import Shoppinglist, User, Shoppinglistitems
     app = FlaskAPI(__name__, instance_relative_config=True)
-    app.config.from_object(app_config['development'])
+    app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
     db.init_app(app)
