165dc0fc7e4bd56ead5145c81d6594a4c3d7c300
Muthomi Mate <muthomimate@gmail.com>
2017-10-16 16:33:11 +0300

    [chore] add flask-cors

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 import os
 from app import create_app
+from flask_cors import CORS
 
 config_name = os.getenv('APP_SETTINGS') # config_name = "development"
 app = create_app(config_name)
 
 if __name__ == '__main__':
     app.run()
+    CORS(app)
