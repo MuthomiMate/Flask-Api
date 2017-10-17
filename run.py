6cc861b5f08f3ab9de8c344142ce6b0c4a0a5b4c
Muthomi Mate <muthomimate@gmail.com>
2017-10-17 08:15:48 +0300

    [Chore]add cors to run.py

--- run.py
+++ run.py
@@ -4,7 +4,8 @@ from flask_cors import CORS
 
 config_name = os.getenv('APP_SETTINGS') # config_name = "development"
 app = create_app(config_name)
+CORS(app)
 
 if __name__ == '__main__':
     app.run()
-    CORS(app)
+    
